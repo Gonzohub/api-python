441debd41192f89da8c72bff23a9a30dbd47a2c5
Gonzohub <58869346+Gonzohub@users.noreply.github.com>
2020-04-10 19:08:07 -0500

    moved folders around and updated files

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "6a6b8763ee601da259a0643aadef9577"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBuKjcy1vB1_nF0m3XEdM69u1l9IOQ-VpE"
